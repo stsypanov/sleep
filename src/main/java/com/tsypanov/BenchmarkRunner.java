2991db9c880b91794650f5a0812a433381da8f91
Сергей Цыпанов <sergei.tsypanov@yandex.ru>
2022-07-08 12:26:40 +0300

    Clean-up

--- src/main/java/com/tsypanov/BenchmarkRunner.java
+++ src/main/java/com/tsypanov/BenchmarkRunner.java
@@ -1,34 +1,30 @@
 package com.tsypanov;
 
 import com.tsypanov.slp.ParkNanosBenchmark;
+import com.tsypanov.slp.ThreadSleep0Benchmark;
+import com.tsypanov.slp.ThreadSleep1Benchmark;
+import com.tsypanov.slp.ThreadSleep2Benchmark;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.openjdk.jmh.runner.options.TimeValue;
 
-import java.util.concurrent.TimeUnit;
-
 public class BenchmarkRunner {
 
   public static void main(String[] args) throws Exception {
-    TimeUnit.NANOSECONDS.sleep(1);
     Options opt = new OptionsBuilder()
             .include(ParkNanosBenchmark.class.getSimpleName())
-//            .include(ThreadSleep1Benchmark.class.getSimpleName())
-//            .include(ThreadSleep2Benchmark.class.getSimpleName())
+            .include(ThreadSleep0Benchmark.class.getSimpleName())
+            .include(ThreadSleep1Benchmark.class.getSimpleName())
+            .include(ThreadSleep2Benchmark.class.getSimpleName())
             .warmupIterations(10)
             .warmupTime(TimeValue.seconds(1))
             .measurementIterations(10)
             .measurementTime(TimeValue.seconds(4))
-            .forks(5) //0 makes debugging possible
+            .forks(4) //0 makes debugging possible
             .shouldFailOnError(true)
-//            .addProfiler(AsyncProfiler.class)
-//            .addProfiler(GCProfiler.class)
-//            .jvmArgsAppend("--add-exports=java.base/sun.net.www=ALL-UNNAMED")
             .build();
 
     new Runner(opt).run();
   }
 }
-
-
