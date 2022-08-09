6d702a7e3eecdc9e15403ce69319535873693b5e
Сергей Цыпанов <sergei.tsypanov@yandex.ru>
2022-08-09 22:54:07 +0300

    Add ThreadSleepBenchmark

---
+++ src/main/java/com/tsypanov/slp/ThreadSleepBenchmark.java
@@ -0,0 +1,49 @@
+package com.tsypanov.slp;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+public class ThreadSleepBenchmark {
+  private final ExecutorService executor = Executors.newFixedThreadPool(1);
+  volatile boolean run;
+
+  @Param({"1", "5", "10", "50", "100"})
+  long delay;
+
+  @Setup(Level.Invocation)
+  public void setUp() {
+    run = true;
+    startThread();
+  }
+
+  @TearDown(Level.Trial)
+  public void tearDown() {
+    executor.shutdown();
+  }
+
+  @Benchmark
+  public int sleep() throws Exception {
+    while (run) {
+      Thread.sleep(1);
+    }
+    return hashCode();
+  }
+
+  private void startThread() {
+    executor.submit(() -> {
+      try {
+        Thread.sleep(delay / 2);
+        run = false;
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+        throw new RuntimeException(e);
+      }
+    });
+  }
+}
